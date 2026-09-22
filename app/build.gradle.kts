14c2451eee8ccfa11a19bb86edd17a9722cfacde
fezanhd-debug-dev <fezanhd@gmail.com>
2026-09-22 15:24:06 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.solarsync.pro"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.solarsync.pro"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
     }
